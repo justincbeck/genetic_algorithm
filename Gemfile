007b7c5c9289fc6b300045767de734995d2bc02b
Justin Beck <justin.beck@meddius.com>
2011-02-03 15:42:35 -0500

    Some refactoring, prep for using idea (to debug) and cleaning up some eroneously scoped variables.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'rspec'
 gem 'cover_me', '>= 1.0.0.rc5'
+gem 'ruby-debug-base19'
--- lib/genetics/algorithm.rb
+++ lib/genetics/algorithm.rb
@@ -11,13 +11,14 @@ module Genetics
   class Algorithm
     attr_accessor :population, :buffer # For testing
     
-    @@GA_POP_SIZE = 2048
-    @@GA_MAX_ITERATIONS = 16384
-    @@RAND_MAX = 32767
-    @@GA_ELITISM_RATE = 0.10
-    @@GA_MUTATION_RATE = 0.25
-    @@GA_MUTATION = @@RAND_MAX * @@GA_MUTATION_RATE
-    @@GA_TARGET = "Hello world!"
+    GA_POP_SIZE = 2048
+    GA_MAX_ITERATIONS = 16384
+    RAND_MAX = 32767
+    GA_ELITISM_RATE = 0.10
+    GA_MUTATION_RATE = 0.25
+    GA_MUTATION = RAND_MAX * GA_MUTATION_RATE
+
+    $ga_target = "Hello world!"
     
     def initialize(output)
       @output = output
@@ -26,13 +27,13 @@ module Genetics
     end
     
     def init_population
-      target_size = @@GA_TARGET.length
+      target_size = $ga_target.length
 
-      @@GA_POP_SIZE.times do
+      GA_POP_SIZE.times do
         citizen = Citizen.new
         
         target_size.times do
-          citizen.string += ((rand(@@RAND_MAX) % 90) + 32).chr
+          citizen.string += ((rand(RAND_MAX) % 90) + 32).chr
         end
 
         @population << citizen
@@ -40,11 +41,11 @@ module Genetics
     end
     
     def calc_fitness(citizen)
-      target = @@GA_TARGET
+      target = $ga_target
       target_size = target.length
       fitness = 0
 
-      (0..(target_size - 1)).each do |j|
+      target_size.times do |j|
         fitness += (citizen.string.getbyte(j) - target.getbyte(j)).abs
       end
       
@@ -55,24 +56,27 @@ module Genetics
       population.sort { |x, y| x.fitness <=> y.fitness }
     end
     
-    def print_best(citizen) # Verified and tested
+    def print_best(citizen)
+      if citizen.fitness < 5
+        @output.puts("Stop here!")
+      end
       @output.puts("Best: #{citizen.string} (#{citizen.fitness})")
     end
     
     def mate
-      esize = (@@GA_POP_SIZE * @@GA_ELITISM_RATE).to_i
-      tsize = @@GA_TARGET.length
+      esize = (GA_POP_SIZE * GA_ELITISM_RATE).to_i
+      tsize = $ga_target.length
       
       elitism(esize)
       
-      (esize..(@@GA_POP_SIZE - 1)).each do |i|
-        i1 = rand(@@RAND_MAX) % (@@GA_POP_SIZE / 2)
-        i2 = rand(@@RAND_MAX) % (@@GA_POP_SIZE / 2)
-        spos = rand(@@RAND_MAX) % tsize
+      (esize..(GA_POP_SIZE - 1)).each do |i|
+        i1 = rand(RAND_MAX) % (GA_POP_SIZE / 2)
+        i2 = rand(RAND_MAX) % (GA_POP_SIZE / 2)
+        spos = rand(RAND_MAX) % tsize
         
         @buffer[i] = Citizen.new(@population[i1].string[0, spos] + @population[i2].string[spos, esize - spos], 0)
         
-        if rand(@@RAND_MAX) < @@GA_MUTATION
+        if rand(RAND_MAX) < GA_MUTATION
           mutate(@buffer[i])
         end
       end
@@ -85,9 +89,9 @@ module Genetics
     end
     
     def mutate(citizen)
-      tsize = @@GA_TARGET.length
-      ipos = rand(@@RAND_MAX) % tsize
-      delta = (rand(@@RAND_MAX) % 90) + 32
+      tsize = $ga_target.length
+      ipos = rand(RAND_MAX) % tsize
+      delta = (rand(RAND_MAX) % 90) + 32
       
       ((citizen.string.getbyte(ipos) + delta) % 122).chr
     end
@@ -98,12 +102,13 @@ module Genetics
       @buffer = temp
     end
 
-    def execute
+    def execute(target)
+      $ga_target = target unless target.nil?
       init_population
       
-      @@GA_MAX_ITERATIONS.times do
+      GA_MAX_ITERATIONS.times do
         
-        @@GA_POP_SIZE.times do |i|
+        GA_POP_SIZE.times do |i|
           @population[i].fitness = calc_fitness(@population[i])
         end
         
